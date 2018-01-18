f9478211bede51e6b51626a8fb8588aed69b6ac5
wangyupo <1106774262@qq.com>
2018-01-18 15:18:39 +0800

    jd

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -2,16 +2,34 @@
     <div>
         <Heador/>
         <Slider/>
+        <Novice/>
+        <Borrow/>
+        <Money/>
+        <Product/>
+        <Life/>
+        <Footer/>
     </div>
 </template>
 
 <script>
 import Heador from "../public/header.vue"
+import Footer from "../public/footer.vue"
 import Slider from "./hslider.vue"
+import Novice from "./novice.vue"
+import Borrow from "./borrow.vue"
+import Money from "./money.vue"
+import Product from "./product.vue"
+import Life from "./life.vue"
 export default {
     components: {
         Heador,
         Slider,
+        Novice,
+        Borrow,
+        Money,
+        Product,
+        Life,
+        Footer,
     },
 }
 </script>
---
+++ app/js/home/product.vue
@@ -0,0 +1,85 @@
+<template lang="html">
+    <Panel title="新品推荐" :class="$style.panel">
+        <section :class="$style.content">
+            <div class="arrow">更多尖货></div>
+            <Slider :options="options" :items="items" cname="product-slider"/>
+        </section>
+    </Panel>
+</template>
+
+<script>
+import Panel from "../core/panel.vue"
+import Slider from "../core/slider.vue"
+export default {
+    components: {
+        Panel,
+        Slider,
+    },
+    data() {
+        return {
+            items: [
+                {
+                    href: "home",
+                    src: "//img12.360buyimg.com/jrpmobile/jfs/t14641/180/1638965003/42812/2f4a85a1/5a54aadbN5269a1f1.jpg?width=335&height=421",
+                },
+                {
+                    href: "home",
+                    src: "//img12.360buyimg.com/jrpmobile/jfs/t14584/14/1620812582/49401/2a98f763/5a54aaeeNde89f852.jpg?width=335&height=421",
+                },
+                {
+                    href: "home",
+                    src: "//img12.360buyimg.com/jrpmobile/jfs/t15670/84/1493061629/40102/9f58843d/5a54aaecNecedcefd.jpg?width=335&height=421",
+                },
+                {
+                    href: "home",
+                    src: "//img12.360buyimg.com/jrpmobile/jfs/t15175/239/1652258993/41684/2827c8e3/5a54ab0fN0c596f31.jpg?width=335&height=421",
+                },
+            ],
+            options: {
+                slidesPerView: 2.3,
+                spaceBetween: 30,
+                freeMode: true,
+            },
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+.product-slider{
+    .swiper-container{
+        box-sizing: border-box;
+        padding: 0 24px;
+        .swiper-slide{
+            a{
+                display: inline-block;
+                width: 100%;
+                img{
+                    width: 100%;
+                    display: block;
+                    height: 314px;
+                    border: 1px solid #fafafa;
+                }
+            }
+        }
+    }
+}
+</style>
+
+<style lang="scss" module>
+    @import "../../css/element.scss";
+    .panel{
+        @include panel;
+        .content{
+            padding-bottom: 40px;
+            position: relative;
+            &>div{
+                position: absolute;
+                font-size: 28px;
+                color: #999;
+                right: 20px;
+                top: -70px;
+            }
+        }
+    }
+</style>
